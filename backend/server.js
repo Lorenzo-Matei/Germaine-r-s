09628b0cf32f7c265b21636862f4d58874e9ebee
Lorenzo <lorenzo.matei@outlook.com>
2022-07-15 21:45:17 -0400

    created server/backend for products data
    used the hooks useState and useEffect to grab and render the products on the server

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import express from "express";
 import data from "./productsData.js";
 
 const app = express();
-app.get("/api/products", (req, res) => {
+app.get("/api/productsData", (req, res) => {
   res.send(data.products);
 });
 
--- src/pages/products-search/products-search-page.component.jsx
+++ src/pages/products-search/products-search-page.component.jsx
@@ -7,16 +7,32 @@ import { IoIosCloseCircleOutline } from "react-icons/io";
 
 import ProductSearchFilters from "../../components/product-search-filters/product-search-filters.component";
 import ProductSearchItem from "../../components/product-search-item/product-search-item.component";
-import productsData from "../../productsData";
+// import productsData from "../../productsData"; // dont use anymore because of the useEffect hook that fetchs it
 
 import "./products-search-page.styles.scss";
 import { data } from "jquery";
+import axios from "axios";
 
 const ProductSearchPage = () => {
   // const[filterMenuWidth, setFilterMenuWidth] = useState('3px');
   const [showFilterMenu, setShowFilterMenu] = useState(false);
   const [filterMenuWidth, setFilterMenuWidth] = useState(null);
   const searchFilterRef = useRef(null);
+
+  //hooks
+  const [productsData, setProducts] = useState([]); // useState() returns an array that contains variable and a function that return values in that variable ; in this case products
+
+  useEffect(() => {
+    //this hook does something everytime something rerenders -> ex. use types something/ click someting
+    const fetchData = async () => {
+      //this is a async functinon that grabs products from backend
+      const result = await axios.get("/api/productsData"); //sends ajax request to the address specified- api
+      setProducts(result.data); // sets state for products by grabbing 'data' from backend with the 'result' function
+    };
+
+    fetchData(); //now that we've define fetchData function we call it within the useEffect.
+  }, []); // 2nd parameter - after the , - specifies when to to do it, in this case its after the first render and only do it once.
+  // therefore useEffect will run this function after the 1st render of this component
 
   const changeIcon = (icon) =>
     icon.classList.toggle(<IoIosCloseCircleOutline />);
@@ -54,7 +70,7 @@ const ProductSearchPage = () => {
           </div>
 
           <div className="search-results-container">
-            {productsData.products.map((product) => (
+            {productsData.map((product) => (
               <ProductSearchItem
                 brand={product.Brand}
                 name={product.name}
@@ -74,14 +90,5 @@ const ProductSearchPage = () => {
 
 export default ProductSearchPage;
 
-{
-  /* {productsData.products.map((product) => (
-              <div className="product" key={product.slug}>
-                <img src={product.image} alt={product.name} />
-                <p>{product.name}</p>
-                <p>{product.price}</p>
-              </div>
-            ))} */
-}
 // <ProductSearchItem />
 // <ProductSearchItem />
